e5b8c5192989be3e628d7ce083ce633bdda94354
wairaguelijah42-star <wairaguelijah42@gmail.com>
2026-04-28 02:34:23 -0400

    Initial secure app

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+
+def main():
+    secret = os.getenv("APP_SECRET")
+    
+    if secret:
+        print(f"Secret loaded securely: {secret[:3]}***")
+    else:
+        print("No secret found")
+
+if __name__ == "__main__":
+    main()
